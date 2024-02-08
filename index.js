04acaf8d93c882dedb7b37e06d6aa89b4137a453
aDog <adrianmg3008@gmail.com>
2024-02-08 12:44:37 -0500

    add exercise 3.6

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ app.get("/api/persons/:id", (req, res) => {
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
+  if (!body.name) return res.status(400).json({ error: "name missing" });
+
+  if (!body.number) return res.status(400).json({ error: "number missing" });
+
+  if (persons.find((person) => person.name === body.name))
+    return res.status(400).json({ error: "name must be unique" });
+
   const person = {
     id: Math.random() * 100,
     name: body.name,
