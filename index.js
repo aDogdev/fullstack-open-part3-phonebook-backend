81f599290ab4ecc761e6370a468d5663dcd7b9e9
aDog <adrianmg3008@gmail.com>
2024-02-16 09:07:17 -0500

    add exercise 3.22

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 const Person = require("./models/person");
 const notFound = require("./middleware/notFound");
 const handleErrors = require("./middleware/handleErrors");
